4385ce3bbe02e2c7a19929ea05692eb2402d7930
Liuyi <860986808@qq.com>
2017-12-31 12:04:19 +0800

    update

--- core/eureka-client/src/main/java/com/liting/aicloud/core/eurekaclient/Client.java
+++ core/eureka-client/src/main/java/com/liting/aicloud/core/eurekaclient/Client.java
@@ -1,33 +1,30 @@
 package com.liting.aicloud.core.eurekaclient;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@Slf4j
+@SpringCloudApplication
 @RestController
 public class Client {
-
-    private static Logger logger = LoggerFactory.getLogger(Client.class);
 
     @GetMapping("/ver.0.1.0-SNAPSHOT")
     public String version(){
         return "";
     }
 
+    @Value("${spring.application.name}")
+    private static String serviceId;
+
     public static void main(String[] args) {
         SpringApplication.run(Client.class, args);
-        logger.info("...............................................................");
-        logger.info("..................{} starts successfully", Client.class.getSimpleName());
-        logger.info("...............................................................");
+        log.info("...............................................................");
+        log.info("..................{} starts successfully", serviceId);
+        log.info("...............................................................");
     }
 
 }
