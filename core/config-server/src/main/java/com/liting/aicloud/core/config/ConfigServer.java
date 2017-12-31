2d492651d07d6c80e5cbde67ef233b5dfca56e51
Liuyi <860986808@qq.com>
2017-12-31 12:08:16 +0800

    update

--- core/config-server/src/main/java/com/liting/aicloud/core/config/ConfigServer.java
+++ core/config-server/src/main/java/com/liting/aicloud/core/config/ConfigServer.java
@@ -1,20 +1,20 @@
 package com.liting.aicloud.core.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
+@Slf4j
+@SpringCloudApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 public class ConfigServer {
-    private static Logger logger = LoggerFactory.getLogger(ConfigServer.class);
 
     @GetMapping("/ver.0.1.0-SNAPSHOT")
     public String version(){
@@ -23,9 +23,9 @@ public class ConfigServer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
-        logger.info("...............................................................");
-        logger.info("..................{} starts successfully", ConfigServer.class.getSimpleName());
-        logger.info("...............................................................");
+        log.info("...............................................................");
+        log.info("..................{} starts successfully", ConfigServer.class.getSimpleName());
+        log.info("...............................................................");
     }
 
 
